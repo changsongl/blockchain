582731da86165dabc6c906834536bdc7a2bac221
lichangsong <298175963@qq.com>
2022-03-14 16:28:23 +0800

    implement blockchain methods

--- blockchain.go
+++ blockchain.go
@@ -5,17 +5,144 @@ import (
 	"crypto/ecdsa"
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"github.com/boltdb/bolt"
 	"log"
+	"os"
 )
 
-// blocksBucket is the bucket name of bolt storage
-const blocksBucket = "blocks"
+const (
+	// dbFileNameFormat is a bolt db file name
+	dbFileNameFormat = "blockchain_%s.db"
+
+	// blocksBucket is the bucket name of bolt storage
+	blocksBucket = "blocks"
+
+	// genesisCoinbaseData is a genesis coinbase data
+	genesisCoinbaseData = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
+
+	// dbFileMode is database file perm
+	dbFileMode = 0600
+
+	// tipDbKey database tip key
+	tipDbKey = "l"
+)
 
 // Blockchain implements interactions with a DB
 type Blockchain struct {
 	tip []byte
 	db  *bolt.DB
+}
+
+// getDBFile returns a bolt database file name
+func getDBFile(nodeID string) string {
+	return fmt.Sprintf(dbFileNameFormat, nodeID)
+}
+
+// CreateBlockchain creates a new blockchain db
+func CreateBlockchain(address, nodeID string) *Blockchain {
+	dbFileName := getDBFile(nodeID)
+	if dbExists(dbFileName) {
+		log.Println("blockchain already exists.")
+		os.Exit(1)
+	}
+
+	cbTx := NewCoinbaseTX(address, genesisCoinbaseData)
+	genesisBlock := NewGenesisBlock(cbTx)
+
+	db, err := bolt.Open(dbFileName, dbFileMode, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(createDatabaseFunc(genesisBlock))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &Blockchain{tip: genesisBlock.Hash, db: db}
+}
+
+// createDatabaseFunc is a function to create a new bolt database
+func createDatabaseFunc(genesis *Block) func(tx *bolt.Tx) error {
+	return func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte(blocksBucket))
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put(genesis.Hash, genesis.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put([]byte(tipDbKey), genesis.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		return nil
+	}
+}
+
+// NewBlockchain creates a new Blockchain with genesis Block
+func NewBlockchain(nodeID string) *Blockchain {
+	dbFileName := getDBFile(nodeID)
+	if !dbExists(dbFileName) {
+		log.Println("no existing blockchain found, create it first.")
+	}
+
+	var tip []byte
+	db, err := bolt.Open(dbFileName, dbFileMode, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		tip = b.Get([]byte(tipDbKey))
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &Blockchain{tip: tip, db: db}
+}
+
+// AddBlock saves the block into the blockchain database
+func (bc *Blockchain) AddBlock(block *Block) {
+	if err := bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		blockInDB := b.Get(block.Hash)
+
+		if blockInDB == nil {
+			return nil
+		}
+
+		blockData := block.Serialize()
+		if err := b.Put(block.Hash, blockData); err != nil {
+			log.Panic(err)
+		}
+
+		lastHash := b.Get([]byte(tipDbKey))
+		lastBlockData := b.Get(lastHash)
+		lastBlock := DeserializeBlock(lastBlockData)
+
+		if block.Height > lastBlock.Height {
+			if err := b.Put([]byte(tipDbKey), block.Hash); err != nil {
+				log.Panic(err)
+			}
+
+			bc.tip = block.Hash
+		}
+
+		return nil
+	}); err != nil {
+		log.Panic(err)
+	}
 }
 
 // Iterator returns a BlockchainIterator
@@ -60,4 +187,13 @@ func (bc *Blockchain) SignTransaction(tx *Transaction, privKey ecdsa.PrivateKey)
 	}
 
 	tx.Sign(privKey, prevTXs)
+}
+
+// dbExists returns whether database file is exists
+func dbExists(dbFile string) bool {
+	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
 }
